fdeaea8bc473b024a4dbf02ac9f511c982a58ef7
Mayukha Mandya Ammangatambu <119781075+MayukhaMandya@users.noreply.github.com>
2025-06-30 21:46:54 -0600

    Time: 0 ms (100.00%) | Memory: 8.7 MB (32.66%) - LeetSync

---
+++ 392-is-subsequence/is-subsequence.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int s_pointer = 0;
+        int t_pointer = 0;
+        while(s_pointer < s.length() && t_pointer < t.length()){
+            if(s[s_pointer] == t[t_pointer])
+                s_pointer++;
+            t_pointer ++;
+        }   
+        return s_pointer == s.length();
+    }
+};
