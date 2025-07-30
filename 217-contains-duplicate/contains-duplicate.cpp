1977fd68394a7f939525a9e6ae18903f7334b340
Mayukha Mandya Ammangatambu <119781075+MayukhaMandya@users.noreply.github.com>
2025-07-29 18:27:06 -0600

    Time: 53 ms (45.01%) | Memory: 90.8 MB (61.63%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int, int> freqmap;
+        for(int num : nums){
+            freqmap[num]++;
+            if(freqmap[num] > 1)
+                return true;
+        }
+        return false;
+
+        
+    }
+};
