e7773f38747909e67b50c84d4b7e741313ea0ec1
Mayukha Mandya Ammangatambu <119781075+MayukhaMandya@users.noreply.github.com>
2026-01-12 11:13:53 -0700

    Time: 382 ms (25.71%) | Memory: 0B (100.00%) - LeetSync

--- 182-duplicate-emails/duplicate-emails.sql
+++ 182-duplicate-emails/duplicate-emails.sql
@@ -1,4 +1,15 @@
 # Write your MySQL query statement below
+
+
+/* METHOD 1 - USINg SELF JOIN
+
 SELECT DISTINCT e1.email as Email
 FROM Person e1 JOIN Person e2
 ON e1.email = e2.email AND e1.id != e2.id
+
+*/
+SELECT email FROM 
+(SELECT email, COUNT(email) as c 
+FROM Person
+GROUP BY email) AS temp
+WHERE c > 1
