e1cc77d0d7cbd41afc2e48e30eeb9cd696e19219
Mayukha Mandya Ammangatambu <119781075+MayukhaMandya@users.noreply.github.com>
2026-01-13 12:10:09 -0700

    Time: 633 ms (12.63%) | Memory: 0B (100.00%) - LeetSync

--- 183-customers-who-never-order/customers-who-never-order.sql
+++ 183-customers-who-never-order/customers-who-never-order.sql
@@ -3,7 +3,13 @@
 1. We need to find customers who never ordered i.e where id in Orders table is NULL
 2. To do that we need to use custId and Join the table Customers and Orders to display
 */
+/* METHOD 1
 SELECT c.name as Customers
 FROM Customers c LEFT JOIN Orders o
 ON o.customerId = c.id 
 WHERE o.id IS NULL
+*/
+
+SELECT name as Customers
+FROM Customers
+WHERE id NOT IN(SELECT customerId FROM Orders)
