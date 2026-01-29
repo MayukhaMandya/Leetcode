3d970658ee0a351df785ba45043c243df3776243
Mayukha Mandya Ammangatambu <119781075+MayukhaMandya@users.noreply.github.com>
2026-01-29 16:55:19 -0700

    Time: 885 ms (68.10%) | Memory: 0B (100.00%) - LeetSync

--- 185-department-top-three-salaries/department-top-three-salaries.sql
+++ 185-department-top-three-salaries/department-top-three-salaries.sql
@@ -1,18 +1,14 @@
 # Write your MySQL query statement below
-
-/*Thought Process:
-1. Window fiucntion - Look at the department Id GROUP BY dept id 
-then have ranking per departnemtn(use partiton by)
-
+/*THOUGHT PROCESS:
+1. Use sub queries
 */
-
-SELECT d.name AS Department, e.name AS Employee, e.salary AS Salary
+SELECT d.name AS Department, x.name AS Employee, x.salary AS Salary
 FROM(
     SELECT e.*,
     DENSE_RANK() OVER(PARTITION BY e.departmentId ORDER BY e.salary DESC) AS rnk
     FROM Employee e
-) e
+) x
 JOIN Department d
-ON e.departmentId = d.id
-WHERE e.rnk <= 3
-
+ON x.departmentId = d.id
+WHERE x.rnk <= 3 
+  
