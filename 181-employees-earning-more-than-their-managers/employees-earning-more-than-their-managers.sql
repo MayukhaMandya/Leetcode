4befccc4ca5fe149060e32e619bb4e6e9881d604
Mayukha Mandya Ammangatambu <119781075+MayukhaMandya@users.noreply.github.com>
2026-05-17 21:39:35 -0600

    Time: 387 ms (50.16%) | Memory: 0B (100.00%) - LeetSync

--- 181-employees-earning-more-than-their-managers/employees-earning-more-than-their-managers.sql
+++ 181-employees-earning-more-than-their-managers/employees-earning-more-than-their-managers.sql
@@ -1,12 +1,11 @@
 # Write your MySQL query statement below
+/*Thought Process
+1. SELF Join table
 
-/* Thought process:
-1. We need access for employee salary and the manager salary
-2. We need to match the manager id 
-3. Select the empoyee ids where emp salary > manager salary 
+
 */
-
-SELECT e.name as Employee 
-FROM Employee e JOIN Employee m
-    ON e.managerId = m.id
-WHERE e.salary > m.salary 
+SELECT e1.name AS Employee FROM Employee e1  
+JOIN Employee e2 
+    ON e1.managerId = e2.id
+WHERE e1.salary > e2.salary
+ 
